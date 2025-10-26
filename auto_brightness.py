548aca1f547ed680956edffb06d17e9ce54823d4
qdongatuw <127705040+qdongatuw@users.noreply.github.com>
2025-10-26 15:36:48 -0500

    downsampling

--- auto_brightness.py
+++ auto_brightness.py
@@ -6,6 +6,19 @@ import numpy as np
 from tqdm import tqdm
 
 
+def downsample_block_average(im, fy=4, fx=4):
+    # im: (T, H, W)
+    T, H, W = im.shape
+    newH = H // fy
+    newW = W // fx
+    # 裁切到能被整除的大小
+    im_cropped = im[:, :newH*fy, :newW*fx]
+    # 转 float 做平均，避免整型截断
+    im_cropped = im_cropped.astype(np.float32, copy=False)
+    im_ds = im_cropped.reshape(T, newH, fy, newW, fx).mean(axis=(2,4))
+    return im_ds
+
+
 root = tk.Tk()
 root.withdraw()
 
@@ -20,13 +33,15 @@ for root_dir, dirs, files in os.walk(selected_folder):
 
 total_num = len(file_paths)
 
-cnt = 0
+
 for file in tqdm(file_paths):
-    cnt += 1
     # tqdm.write(f'Proseeing {os.path.basename(file)} : {cnt}/{total_num}')
 
-    output_file = f'{os.path.splitext(file)[0]}_enhanced.tif'
+    output_file = f'{os.path.splitext(file)[0]}_enhanced_downsampling.tif'
     im = tiff.imread(file)
+    if im.shape[1] > 1000:
+        im = downsample_block_average(im, fy=4, fx=4)
+
     hist, bins = np.histogram(im, 255)
 
     threshold = im.size / 5000
